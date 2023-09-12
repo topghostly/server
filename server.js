9d42603f347f919f8ab7a46b0ff39af1ac20b90e
David <topghostly@gmail.com>
2023-09-12 18:40:59 +0100

    first commit

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
-const express = require('express')
-const authRoutes = require('./routes/Auth')
-const cors = require('cors')
-const bodyParser = require("body-parser")
-const mongoose = require("mongoose")
-const cookieParser = require("cookie-parser")
-require("dotenv").config()
+const express = require("express");
+const authRoutes = require("./routes/Auth");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-console.log(process.env.MONGODB_URI)
-mongoose.connect('mongodb+srv://Ayinla:Hucrux0327@cluster0.em93uoi.mongodb.net/Account?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then((responce)=>{
-    app.listen(4000, ()=>{
-        console.log("Fire on 🚀🔥")
-    })    
-}).catch((err)=>{
-    console.log(err)
-})
+console.log(process.env.MONGODB_URI);
+mongoose
+  .connect(
+    "mongodb+srv://Ayinla:Hucrux0327@cluster0.em93uoi.mongodb.net/Account?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((responce) => {
+    app.listen(4000, () => {
+      console.log("Fire on 🚀🔥");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-
-app.use(cors())
-app.use(bodyParser.json())
-app.use(cookieParser())
-
-
-
-app.use('/auth', authRoutes)
+app.use("/auth", authRoutes);
